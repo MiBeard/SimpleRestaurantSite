94824cf11701c12d8c5ed1469e279375dc56ce34
Your Name Here <ewise@test.comclear>
2018-08-24 22:26:38 -0400

    SimpleRestaurantSite

---
+++ SimpleRestaurantSite/README.md
@@ -0,0 +1,1 @@
+# RestaurantVersion2018
---
+++ SimpleRestaurantSite/jScript.js
@@ -0,0 +1,91 @@
+// var name;
+
+// name = prompt("Whats your name?");
+
+// var yn;
+
+// yn= confirm(myName(name)+", how do you like the website??");
+
+
+// if(yn){
+//     alert("I am glad you like it");
+// }else if(!yn){
+//     prompt("What would you recommend?")
+// }
+
+// alert("Thanks for your input!");
+
+// function myName(theName){
+//     alert("Hello "+theName);
+// }
+//-------------------
+// function countingCharacters(stringToCount){
+
+// console.log(stringToCount+" has "+stringToCount.length+" characters.")
+
+// }
+
+// countingCharacters("Matt");
+
+// var bond="This sentence is a test";
+
+// countingCharacters(bond);
+//--------------------
+
+
+// function countingCharacters(seq, item){
+
+// var x = 0;
+
+// for(var y=0;y<seq.length;y++){
+
+// if(seq[y]==item){
+//     x++;
+// }
+// }
+// console.log("Your sequence: "+ seq);
+// console.log("Your character: "+ item);
+// console.log(item+" appears "+x+" time(s) "+ " in "+seq);
+// }
+// countingCharacters("xooooxoxooxooxoxoxo", "x");
+// -------------------
+
+
+// function rollDice(max,min){
+
+//     return Math.ceil(Math.random()*(1+max-min));
+
+// }
+
+// for(var x =0; x<10;x++){
+
+//     console.log(rollDice(3,1));
+// }
+
+// function sumOfNums(num1,num2){
+
+// var total = num1+num2;
+
+// return total;
+
+// }
+
+// console.log(sumOfNums(4,5));
+// -------------------
+
+// var friends = ["Aneki", "Quell", "Clarity", "Sleepy", "Roghar", "DM Crimson"];
+
+// var team1 = new Array();
+// var team2 = new Array();
+
+// for(var i=0;i<friends.length;i++){
+//     if(i%2==0){
+//         team1.push(friends[i]);
+//     }else if (i%2!=0){
+//         team2.push(friends[i]);
+//     }
+// }
+// console.log(team1);
+// console.log(team2);
+// ----------------------
+
